d20c92597c709d6d085c248da43a9318c0112b14
thami <thamizh.ai05@gmail.com>
2025-09-11 22:02:09 +0530

    11/09 Solved

--- src/firstpack/PrimeNum.java
+++ src/firstpack/PrimeNum.java
@@ -7,6 +7,14 @@ public class PrimeNum {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter a Number: ");
         int n=sc.nextInt();
-        System.out.println(n);
+        if (n <= 1) {System.out.println("Not"); return;}
+        if (n == 2) {System.out.println("Yes"); return;}
+        if (n % 2 == 0) {System.out.println("Not"); return;}
+
+        for(int i=3;i*i<n;i++){
+            if (n % i == 0) {System.out.println("Not");
+            return;}
+        }
+        System.out.println("Yes");
     }
 }
