27568616796750ce1e1de625ad17093d8bc9895d
thami <thamizh.ai05@gmail.com>
2025-09-23 21:58:52 +0530

    Day Task

---
+++ src/Daily_Task/Day5.java
@@ -0,0 +1,24 @@
+package Daily_Task;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static java.util.Arrays.*;
+
+public class Day5 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter length of array: ");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        System.out.print("Enter Array Element: ");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+        }
+        System.out.println("The Sum of Given Array is "+sum);
+    }
+}
