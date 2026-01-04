259b377e824c65bd7d2d7d0a11c19429a8d87f5f
Thamizharasan K <thamizh.ai05@gmail.com>
2026-01-04 22:45:09 +0530

    Add Uniquedigit program to count unique digits

---
+++ src/java-programs/Uniquedigit.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Uniquedigit {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter Starting Range: ");
+		int num=sc.nextInt();
+		 int[] freq = new int[10]; // 0 to 9 digits
+        int count = 0;
+        while (num != 0) {
+            int digit = num % 10;
+            if (freq[digit] == 0) {   // first time occurrence
+                count++;
+                freq[digit] = 1;
+            }
+            num = num / 10;
+        }
+        System.out.println("Unique digit count = " + count);
+	}
+}
